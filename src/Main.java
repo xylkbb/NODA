be8f40a06c09fe46cddf4b5d652f9a3e2daacc14
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-04 13:38:38 -0400

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
+    public static boolean isPalindrome(int number){
+        number = Math.abs(number);
+
+        int numberClone = number;
+        int reverse = 0;
+
+        while(number > 0){
+            int lastDigit = number % 10;
+            reverse *= 10;
+            reverse += lastDigit;
+            number /= 10;
+
+        }
+
+        return reverse == numberClone;
+
+
+
+    }
+
 }
